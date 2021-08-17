385846045e3f991b5bd501e5916bd2d72965544a
Tobias Wölfel <tobias.woelfel@mailbox.org>
2021-08-17 17:05:40 +0200

    Use fault injection in example
    Add dependency in a simple example to fault injection.
    Utilize fault injection and insert a fault at a specific time and place.

--- example/simple/cpp/top.cc
+++ example/simple/cpp/top.cc
@@ -5,12 +5,11 @@
 #include <verilated_vcd_c.h>
 
 #include "Vtop.h"
+#include "fault_injection.h"
 
 int main(int argc, char *argv[], char **env) {
 
-
   const std::unique_ptr<VerilatedContext> cp{new VerilatedContext};
-
 
   cp->traceEverOn(true);
 
@@ -25,6 +24,10 @@ int main(int argc, char *argv[], char **env) {
   top->trace(tfp, 99);
   tfp->open("trace.vcd");
 
+  // Creat a fault injection with the width of top->fi_combined, an activation
+  // in clyce 8 and at top->fi_combined[42].
+  FaultInjection fi = FaultInjection(70, 8, 42);
+
   bool sim_done = false;
   while (!sim_done) {
     // Alternate clock
@@ -37,6 +40,14 @@ int main(int argc, char *argv[], char **env) {
         top->count = true;
       }
     }
+
+    if (top->clk) {
+      // Update the fault insertion.
+      // If the desired clock cycle is reached, a fault is injected for one
+      // cycle.
+      fi.UpdateInsert(top->fi_combined);
+    }
+
     top->eval();
 
     tfp->dump(cp->time());
